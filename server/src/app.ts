6813c52d4b7f379e6460c94d924696aa0b38d7a1
edocai <edocaai@hotmail.com>
2023-04-03 18:59:01 +0100

    adding middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
+import morgan from 'morgan';
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('dotenv').config();
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.get('/api/v1/restaurants', (req: Request, res: Response) => {
