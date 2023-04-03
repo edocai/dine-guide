5068c73c72ba5d20766b3c767637fb1eac7596b0
edocai <edocaai@hotmail.com>
2023-04-03 19:09:34 +0100

    created test endpoints

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,8 +9,25 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
+//GET: Get All Restaurants
 app.get('/api/v1/restaurants', (req: Request, res: Response) => {
   res.send('Successful');
+});
+
+//GET: Get a Restaurant
+app.get('/api/v1/restaurants/:id', (req: Request, res: Response) => {
+  console.log(req.params);
+});
+
+//POST: Create a Restaurant
+app.get('/api/v1/restaurants', (req: Request, res: Response) => {
+  console.log(req.body);
+});
+
+//PUT: Update Restaurants
+app.put('/api/v1/restaurants/:id', (req: Request, res: Response) => {
+  console.log(req.params);
+  console.log(req.body);
 });
 
 export default app;
