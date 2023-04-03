cbd51136f70fce8419ab9e0e4677f23a9eb74003
edocai <edocaai@hotmail.com>
2023-04-03 18:32:09 +0100

    first commit

---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/api/v1/restaurants', (req: Request, res: Response) => {
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+  });
+});
+
+export default app;
