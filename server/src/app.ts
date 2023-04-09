abf1e4d4d7846937bc0ff4c94bb82bc4dd1a058d
edocai <edocaai@hotmail.com>
2023-04-09 17:28:29 +0100

    client side setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
 import { pool } from './db';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
 require('dotenv').config();
 
 const app = express();
 
+//middleware
 app.use(morgan('dev'));
 app.use(express.json());
 
@@ -30,7 +29,10 @@ app.get('/api/v1/restaurants', async (req: Request, res: Response) => {
 //GET: Get a Restaurant
 app.get('/api/v1/restaurants/:id', async (req: Request, res: Response) => {
   try {
-    const results = await pool.query('SELECT * from restaurants WHERE id = $1', [req.params.id]);
+    const results = await pool.query(
+      'SELECT * from restaurants WHERE id = $1',
+      [req.params.id],
+    );
     res.status(200).json({
       status: 'success',
       data: {
@@ -45,11 +47,14 @@ app.get('/api/v1/restaurants/:id', async (req: Request, res: Response) => {
 //POST: Create a Restaurant
 app.post('/api/v1/restaurants', async (req: Request, res: Response) => {
   try {
-    const results = await pool.query('INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) returning *', [req.body.name, req.body.location, req.body.price_range]);
+    const results = await pool.query(
+      'INSERT INTO restaurants (name, location, price_range) VALUES ($1, $2, $3) returning *',
+      [req.body.name, req.body.location, req.body.price_range],
+    );
     res.status(201).json({
       status: 'created',
       data: {
-        restaurant: results.rows[0], 
+        restaurant: results.rows[0],
       },
     });
   } catch (error) {
@@ -60,11 +65,14 @@ app.post('/api/v1/restaurants', async (req: Request, res: Response) => {
 //PUT: Update Restaurants
 app.put('/api/v1/restaurants/:id', async (req: Request, res: Response) => {
   try {
-    const results = await pool.query('UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 returning *', [req.body.name, req.body.location, req.body.price_range, req.params.id]);
+    const results = await pool.query(
+      'UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 returning *',
+      [req.body.name, req.body.location, req.body.price_range, req.params.id],
+    );
     res.status(200).json({
       status: 'updated',
       data: {
-        restaurant: results.rows[0], 
+        restaurant: results.rows[0],
       },
     });
   } catch (error) {
@@ -72,9 +80,12 @@ app.put('/api/v1/restaurants/:id', async (req: Request, res: Response) => {
   }
 });
 
+//DELETE: Delete a Restaurant
 app.delete('/api/v1/restaurants/:id', async (req: Request, res: Response) => {
   try {
-    const results = await pool.query('DELETE FROM restaurants WHERE id = $1', [req.params.id]);
+    const results = await pool.query('DELETE FROM restaurants WHERE id = $1', [
+      req.params.id,
+    ]);
     res.status(204).json({
       status: 'deleted',
       data: {
