6e447bf272e4ef28aa6c79911bc55c9772f47a88
Ali <edocaai@hotmail.com>
2023-07-25 09:32:21 +0100

    adding the update functionality

--- client/src/routes/UpdateRestaurantPage.tsx
+++ client/src/routes/UpdateRestaurantPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import UpdateRestaurants from '../components/UpdateRestaurants';
 
 const UpdateRestaurantPage = () => {
   return (
     <>
       <h1>Update Restaurant</h1>
+      <UpdateRestaurants/>
     </>
   );
 };
